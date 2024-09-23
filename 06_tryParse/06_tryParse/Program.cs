f6cc937a45ec5db2a36d4ece83e96594add5161f
KatJ. <jurk.kat@gmail.com>
2024-09-23 14:21:54 +0200

    newer versions of the code

--- 06_tryParse/06_tryParse/Program.cs
+++ 06_tryParse/06_tryParse/Program.cs
@@ -1,24 +1,13 @@
-﻿// How to get a random number:
-Random random = new Random();
-int randomNumber = random.Next(1, 11);
-
-// Try.Parse
+﻿// Try.Parse
 Console.WriteLine("Give me a number:");
-string inputString = Console.ReadLine(); // Corrected variable name from 'intputString' to 'inputString'
+string inputString = Console.ReadLine();
 
 int num = 0;
 bool isNumber = int.TryParse(inputString, out num);
 
 if (isNumber)
 {
-    if (num == randomNumber)
-    {
-        Console.WriteLine("Congratulations! You guessed right!");
-    }
-    else
-    {
-        Console.WriteLine("Luck was not on your side. Try again!");
-    }
+    Console.WriteLine("Yay! You have entered a number!");
 }
 else
 {
@@ -29,7 +18,7 @@ else
 // num++;
 // Console.WriteLine($"User entered number +1 {num}");
 
-Console.ReadKey(); // Optional, only if you want the console to wait for a key press before closing
+Console.ReadKey();
 
 
 
