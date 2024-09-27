1932b7e5f94c0dd01b9cdcaf2029b766e2d76a6e
KatJ. <jurk.kat@gmail.com>
2024-09-27 18:38:57 +0200

    Guess the number app

---
+++ 10_guessTheNumberApp/10_guessTheNumberApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// Guess the number Application
+
+int secretNumber = 42;
+int userGuess = 0;
+
+Console.WriteLine("Write a number I am thinking of between 1 and 100");
+
+while (userGuess != secretNumber)
+{
+    Console.WriteLine("Enter your guess");
+    userGuess = int.Parse(Console.ReadLine());
+    if (userGuess < secretNumber)
+    {
+        Console.WriteLine("Too low! Try again!");
+    }
+    if (userGuess > secretNumber)
+    {
+        Console.WriteLine("Too high! Try again!");
+    }
+    else
+    {
+        Console.WriteLine("Congratulations! You guessed the number correctly!");
+    }
+}
+
+Console.ReadLine();
+
+
