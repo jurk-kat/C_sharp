0676dd5c21e6d8f8053083b961cc63525cfd100f
KatJ. <jurk.kat@gmail.com>
2024-10-01 17:18:07 +0200

    break and continue in the loop

---
+++ 13_Break&Continue/13_Break&Continue/Program.cs
@@ -0,0 +1,23 @@
+﻿// break
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 3)
+    {
+        Console.WriteLine("I have had enough");
+        break; // gets out of this loop. Useful if we need for example a user input and it will decide depending on that
+    }
+
+}
+
+// continue
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 3)
+    {
+        continue; // does not stop the loop, but the rest of the code in the for loop will not be executed
+    }
+    Console.WriteLine("Only if not continued");
+
+}
